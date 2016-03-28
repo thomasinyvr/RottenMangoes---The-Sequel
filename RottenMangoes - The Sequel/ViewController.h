208f39a1420709e3067f85a18d30f62cb932959b
thomasinyvr <tomfriesman@gmail.com>
2016-03-28 12:28:49 -0700

    Initial Commit

---
+++ RottenMangoes - The Sequel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenMangoes - The Sequel
+//
+//  Created by Thomas Friesman on 2016-03-28.
+//  Copyright © 2016 Thomas Friesman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RottenMangoes - The Sequel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RottenMangoes - The Sequel
+//
+//  Created by Thomas Friesman on 2016-03-28.
+//  Copyright © 2016 Thomas Friesman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
